97104e6766bcb9bcaa8fd8091497dfe2ee998cc9
Jen Arevalo <jensif@users.noreply.github.com>
2022-02-17 13:16:14 -0500

    Update _document.js
    Remove "clsx"

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main } from "next/document";
 import React from 'react';
-import clsx from "clsx";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
