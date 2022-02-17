fffe55f0d3c47e7eef04cbd4f608c3075d9a671e
jensif <jensarevalo@gmail.com>
2022-02-17 13:39:13 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
 }
 
-const debug = process.env.NODE_ENV !== 'production'
-
-module.exports = {
-  assetPrefix: !debug ? '/thanku-nextjs/' : '',
-}
+// const debug = process.env.NODE_ENV !== 'production'
+//
+// module.exports = {
+//   assetPrefix: !debug ? '/thanku-nextjs/' : '',
+// }
