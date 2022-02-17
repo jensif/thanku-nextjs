baf7130e877e923772386b2801ca55d59f66bf6d
jensif <jensarevalo@gmail.com>
2022-02-17 13:58:41 -0500

    Sides and fixins

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Script from 'next/script';
 
 import "./styles.css";
 
@@ -8,22 +7,9 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" httpEquiv="width=device-width, initial-scale=1" />
-        <title>Jen Arevalo UI/X Designer & Developer</title>
+        <title>Jen Arevalo</title>
       </Head>
       <Component {...pageProps} />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=UA-90347905-1"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-90347905-1');
-        `}
-      </Script>
     </>
   );
 }
